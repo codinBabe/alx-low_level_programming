3825b15bfbc1499368327ec6e286a5407f03834e
codinBabe <teboola2@gmail.com>
2023-03-16 12:31:21 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,21 +7,16 @@
  */
 int main(void)
 {
-	char charType;
-	int intType;
-	long int longintType;
-	long long int longlongintType;
-	float floatType;
 
-	printf("Size of a char: %c byte(s)\n", sizeof(charType));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
 
-	printf("Size of a int: %d byte(s)\n", sizeof(intType));
+	printf("Size of a int: %d byte(s)\n", sizeof(int));
 
-	printf("Size of a long int: %ld byte(s)\n", sizeof(longintType));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 
-	printf("Size of a long long int: %lld byte(s)\n", sizeof(longlongType));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 
-	printf("Size of a float: %f byte(s)\n", sizeof(floatType));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
 	return (0);
 }

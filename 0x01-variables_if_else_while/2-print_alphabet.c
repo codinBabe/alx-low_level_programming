f32075aaffaaafc6e4edc1fde0c070a5d74757bc
codinBabe <teboola2@gmail.com>
2023-03-17 16:46:16 -0700

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point for char that print alphabet using puchar and
+ * loop
+ *
+ * Return: Always zero (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 97; x <= 122;  x++)
+	{
+		putchar(x);
+
+		putchar('\n');
+	}
+
+	return (0);
+}

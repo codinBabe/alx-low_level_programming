219168d973d737857e277772b48b66a41337bb2f
codinBabe <teboola2@gmail.com>
2023-03-17 20:56:42 -0700

    print single digit combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point diplay single digit combination
+ *
+ * Return: Always zero (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+
+		if (i < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}

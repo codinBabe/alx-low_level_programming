4c86678fbeca63a89dd56298f9fc666f978f1d90
codinBabe <teboola2@gmail.com>
2023-03-17 18:50:27 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point char that print integers from base 10 to 0
+ *
+ * Return: Always zero (Sucess)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + 48);
+	}
+
+	putchar('\n');
+	return (0);
+}

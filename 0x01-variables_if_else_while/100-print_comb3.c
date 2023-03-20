13af9d42c363d9575a2a32a3933becb86bcac456
codinBabe <teboola2@gmail.com>
2023-03-19 20:26:47 -0700

    combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point of two digit combination
+ *
+ * Return: Always zero (Success)
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 48; x < 58; x++)
+	{
+		for (y = 49; y < 57; y++)
+		{
+			if (y > x)
+			{
+				putchar(x);
+				putchar(y);
+					if ((x < 56) || (y < 57))
+					{
+						putchar(44);
+						putchar(32);
+					}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}

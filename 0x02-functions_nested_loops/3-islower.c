9eafeb4f5af7b6844cd7dc06e14dc0f5ebb15c62
codinBabe <teboola2@gmail.com>
2023-03-21 09:51:12 -0700

    add islower file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - check for lower case
+ *@c: The character to be checked
+ * Return: On true 1 is returned
+ * on false 0 is returned
+ */
+int _islower(int c)
+{
+
+	for (c = 97; c <= 122; c++)
+	{
+		return (1);
+	}
+	return (0);
+}
+

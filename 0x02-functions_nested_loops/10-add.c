3f2789a286ace607365ed409e8a80955646c31d3
codinBabe <teboola2@gmail.com>
2023-03-21 20:06:15 -0700

    added sum file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *add - addition of two numbers
+ *@a: int type number
+ *@b: int type number
+ * Return: sum of the numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}

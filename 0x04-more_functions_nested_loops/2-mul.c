78dd50e591ae2529e6e560181357117d8438bdc6
codinBabe <teboola2@gmail.com>
2023-03-23 06:32:47 -0700

    added 2-mul file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @a: The first variable
+ * @b: The second variable
+ * 
+ * Return: The mutiplied value of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

776838355d472a6cfad8f0966d37b13cdf78ee73
codinBabe <teboola2@gmail.com>
2023-03-27 15:50:02 -0700

    add reset98 file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - This function takes a pointer to an int and update to 98
+ *@n: pointer to dereference
+ *
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

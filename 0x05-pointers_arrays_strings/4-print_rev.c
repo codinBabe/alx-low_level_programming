7731615eee3eafc08476b61054e96aa79c81d5e8
codinBabe <teboola2@gmail.com>
2023-03-27 18:16:23 -0700

    add printrev file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - This function print reverse string
+ * @s: the character to be tested
+ *
+ * Return: reverse string
+ */
+void print_rev(char *s)
+{
+	int i, j;
+
+	j = 0;
+	for (i = 0; s[i] != '\0'; i++)
+		j++;
+	for (i = j; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}

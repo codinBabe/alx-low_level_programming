7eea874e55262d89ec60c912977c58961a68f698
codinBabe <teboola2@gmail.com>
2023-03-29 14:02:47 -0700

    add strcmp file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * _strcmp - This function compare two string
+ * @s1: The first string
+ * @s2: The second string
+ *
+ * Return: 0 if s1 == s2, 1 if s1 > s2 and -1 if s2 > s1
+ */
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
@@ -8,6 +15,6 @@ int _strcmp(char *s1, char *s2)
 		s1++;
 		s2++;
 	}
-	i = (*s1 == *s2)?0:(*s1 > *s2)?1:-1;
+	i = (*s1 == *s2) ? 0 : (*s1 > *s2) ? 1 : -1;
 	return (i);
 }

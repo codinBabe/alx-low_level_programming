6193ad88db487faa810398bdb5abebc73c4bc2d6
codinBabe <teboola2@gmail.com>
2023-03-29 02:11:30 -0700

    add 0-strcat file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,22 @@
 #include "main.h"
+/**
+ * _strcat - This function concatenate 2 strings
+ * @dest: the first string
+ * @src: the second string
+ * Return: A pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; src[j] != '\0'; ++j, ++i)
+	{ dest[i] = src[j];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,1 +1,10 @@
-More pointers, arrays and strings task
+## More pointers, arrays and strings task
+---
+- 0-strcat.c
+- 1-strncat.c
+- 2-strncpy.c
+- 3-strcmp.c
+- 4-rev_array.c
+- 5-string_toupper.c
+- 6-cap_string.c
+- 7-leet.c

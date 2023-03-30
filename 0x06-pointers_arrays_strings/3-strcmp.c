b70e3e8529e1a1ce510c6f97a055f5a9f569a200
codinBabe <teboola2@gmail.com>
2023-03-30 03:01:45 -0700

    add 3strcmp file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,17 +4,13 @@
  * @s1: The first string
  * @s2: The second string
  *
- * Return: 0 if s1 == s2, 1 if s1 > s2 and -1 if s2 > s1
+ * Return: 0 if s1 == s2, postive number in ascii if s1 > s2,
+ * negative number in ascii if s2 > s1.
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	while (*s1 == *s2 && *s1)
+		++s1, ++s2;
 
-	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
-	{
-		s1++;
-		s2++;
-	}
-	i = (*s1 == *s2) ? 0 : (*s1 > *s2) ? 15 : -15;
-	return (i);
+	return (*s1 - *s2);
 }

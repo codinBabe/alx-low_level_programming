1a62ed33a55972d64ad8383722c5a1aa2390ecd3
codinBabe <teboola2@gmail.com>
2023-04-03 19:39:05 -0700

    add printdiagsums file

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - This function print sum of two diagonal square matrix
+ * @a: pointer to array of int
+ * @size: size of the matrix
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, sum1, sum2;
+
+	sum1 = 0;
+	sum2 = 0;
+	for (i = 0; i < size; i++)
+	{
+		sum1 += *(a + i * size + i);
+		sum2 += *(a + i * size + (size - 1 - i));
+	}
+	printf("%i, %i\n", sum1, sum2);
+}

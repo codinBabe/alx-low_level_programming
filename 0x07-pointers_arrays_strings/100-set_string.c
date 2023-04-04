a37be7eaaa631e8cf31aad4f80df1c5e081449d1
codinBabe <teboola2@gmail.com>
2023-04-04 10:01:46 -0700

    update set string file

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * set_string - This function set the value of a pointer to a char
+ * @s:a double pointer
+ * @to:address pointed to by s
+ */
 void set_string(char **s, char *to)
 {
 	*s = to;

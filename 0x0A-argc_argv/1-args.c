891a37213dd82bb2cb70beff189f2072428f92e8
codinBabe <teboola2@gmail.com>
2023-04-07 22:52:25 -0700

    add nargs and args file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - This function print number of argument passed into it
+ * @argc: argument count
+ * @argv: argument vector
+ * Return:number of arg passed
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+
+		printf("%d\n", (argc - 1));
+
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,4 +2,6 @@
 #define MAIN_H
 #include <stdio.h>
 int _putchar(char c);
+int main(int argc, char *argv[]);
+int main(int argc, char __attribute__((__unused__)) *argv[]);
 #endif /* MAIN_H */

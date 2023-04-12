a80a1047e1e86ac7842e40957458565cbdae5d70
codinBabe <teboola2@gmail.com>
2023-04-11 18:08:19 -0700

    completed task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,18 +12,18 @@ char *_strdup(char *str)
 	char *copy;
 
 	if (str == NULL)
-		return NULL;
-	
+		return (NULL);
+
 	while (str[len] != '\0')
 		len++;
-	
+
 	copy = malloc(sizeof(char) * (len + 1));
 	if (copy == NULL)
-		return NULL;
-	
+		return (NULL);
+
 	for (i = 0; i <= len; i++)
 		copy[i] = str[i];
-	
-	return copy;
+
+	return (copy);
 
 }

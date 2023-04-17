7d5f6179a181e96c84b195c79a245dc04c1af2cd
codinBabe <teboola2@gmail.com>
2023-04-17 15:04:19 -0700

    update task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - define structure for dog
+ * @name: first member
+ * @owner: second member
+ * @age: age of dog
+ *
+ * Description: created a structure for dog with its name,age and owners name.
+ */
 struct dog
 {
 	char *name;

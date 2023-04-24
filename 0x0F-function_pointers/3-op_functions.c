1c73017dfdbd69b9deff05928a2c808f424ccab5
codinBabe <teboola2@gmail.com>
2023-04-24 06:48:22 -0700

    add task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - This function add 2 numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - This function substract 2 numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: substracted value of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - This function multiply 2 numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: multiple a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - This function divide 2 numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - This function check for remainder 2 numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: remainder of a divided by b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

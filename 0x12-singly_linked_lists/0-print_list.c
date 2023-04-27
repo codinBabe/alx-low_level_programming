37fe3c0ef0b0e277e425c59cace3ca9a4cd5b9c9
codinBabe <teboola2@gmail.com>
2023-04-27 16:16:13 -0700

    add task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,9 @@
 #include "lists.h"
+/**
+ * print_list - function that prints all the elements of a list_t list.
+ * @h: pointer to head
+ * Return:number of nodes
+ */
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
@@ -13,4 +18,4 @@ size_t print_list(const list_t *h)
 		h = h->next;
 	}
 	return (count);
-}	
+}

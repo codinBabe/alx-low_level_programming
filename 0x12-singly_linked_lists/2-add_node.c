ca85a9de1dea7f0f9b4ff9027e921287286bb7e6
codinBabe <teboola2@gmail.com>
2023-04-28 01:51:19 -0700

    add task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,10 @@
 #include "lists.h"
+/**
+ * add_node - This function adds a new node at the beginning of a list_t list.
+ * @head: pointer to beginning of list_t
+ * @str: string in list_t
+ * Return:the address of the new element, or NULL if it failed
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));

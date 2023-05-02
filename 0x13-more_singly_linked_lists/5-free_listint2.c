12f6eb14e916c572f6eaae0d5f1f6c600b049282
codinBabe <teboola2@gmail.com>
2023-05-02 11:04:41 -0700

    update task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,10 @@ void free_listint2(listint_t **head)
 	listint_t *current_node = *head;
 	listint_t *next_node;
 
-	while (current_node != NULL)
+	if (current_node == NULL)
+		return;
+	
+	while (current_node)
 	{
 		next_node = current_node->next;
 		free(current_node);

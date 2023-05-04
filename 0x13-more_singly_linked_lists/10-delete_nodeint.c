1425589353827008843989107a1ffc055a9b2aeb
codinBabe <teboola2@gmail.com>
2023-05-03 20:49:42 -0700

    add task 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - function that deletes the node at index
+ * index of a listint_t linked list.
+ * @head: pointer to head of list
+ * @index:index of the node that should be deleted.
+ * Return:1 if it succeeded, -1 if it failed
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp = *head;
+	listint_t *node_to_delete;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = temp->next;
+		free(temp);
+		return (1);
+	}
+	while (i < (index - 1))
+	{
+		if (temp == NULL || temp->next == NULL)
+			return (-1);
+		temp = temp->next;
+		i++;
+	}
+	node_to_delete = temp->next;
+	if (node_to_delete == NULL)
+		return (-1);
+	temp->next = node_to_delete->next;
+	free(node_to_delete);
+	return (1);
+}

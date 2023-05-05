284e5c6ca8bbddf5350376c29262338167bcf774
codinBabe <teboola2@gmail.com>
2023-05-04 20:43:06 -0700

    add task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * set_bit - function that sets the value of a bit to 1 at a given index.
+ * @n:number to print
+ * @index:is the index, starting from 0 of the bit you want to set
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask, result;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	mask = 1 << index;
+	result = *n | mask;
+	*n = result;
+
+	return (1);
+}

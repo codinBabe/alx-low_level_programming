b6d08dc8c4be60ac6777924a471e7a197ba684ca
codinBabe <teboola2@gmail.com>
2023-05-10 16:07:19 -0700

    update task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,8 +7,9 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd, dest_fd, bytes, src_fd, close_dest;
-	char buffer[BUFSIZ];
+	int fd, dest_fd, bytes_write, src_fd, close_dest;
+	ssize_t bytes_read = 1024;
+	char buffer[1024];
 
 	if (argc != 3)
 	{
@@ -16,34 +17,42 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	fd = open(argv[1], O_RDONLY);
-	if (fd < 0)
+	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((bytes = read(fd, buffer, BUFSIZ)) > 0)
+	if (dest_fd == -1)
 	{
-		if (dest_fd < 0 || write(dest_fd, buffer, bytes) != bytes)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	if (bytes_read == 1024)
+	{
+		bytes_read = read(fd, buffer, 1024);
+		bytes_write = write(dest_fd, buffer, bytes_read);
+		if (bytes_read == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		if (bytes_write == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
-	if (bytes < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
 	src_fd = close(fd);
 	close_dest = close(dest_fd);
-	if (src_fd < 0 || close_dest < 0)
+	if (src_fd == -1)
 	{
-		if (src_fd < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		if (close_dest < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	if (close_dest == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_fd);
 		exit(100);
 	}
 	return (0);

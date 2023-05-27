e51082b49daef88b7f164e098fa9461b8fb392c8
codinBabe <teboola2@gmail.com>
2023-05-27 14:56:22 -0700

    remove folders

--- 0x0A-argc_argv/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
-int _putchar(char c);
-int main(int argc, char *argv[]);
-int main(int argc, char __attribute__((__unused__)) *argv[]);
-#endif /* MAIN_H */

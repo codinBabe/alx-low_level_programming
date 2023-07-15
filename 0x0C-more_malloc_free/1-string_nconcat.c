72154dd51dd3f7f5ce8659af50fb3bdcc24400cd
codinBabe <teboola2@gmail.com>
2023-07-15 04:44:38 -0700

    add task 0

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ *string_nconcat - This function that concatenates two strings.
+ *@s1:source string
+ *@s2:dest string
+ *@n:string length
+ *Return:
+ */
+ char *string_nconcat(char *s1, char *s2, unsigned int n)

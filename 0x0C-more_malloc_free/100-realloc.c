83839dae48fd993f8a7fc9651de1881357f3d812
codinBabe <teboola2@gmail.com>
2023-07-15 08:13:31 -0700

    fix betty

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,23 +15,21 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == NULL)
 	{
-	       new_block = malloc(new_size);
-
+		new_block = malloc(new_size);
 		return (new_block);
 	}
+
 	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
+
 	new_block = malloc(new_size);
 	if (new_block == NULL)
 		return (NULL);
 
-	if (old_size < new_size)
-		min_size = old_size;
-	else
-		min_size = new_size;
+	min_size = (old_size < new_size) ? old_size : new_size;
 
 	for (i = 0; i < min_size; i++)
 		new_block[i] = ((unsigned char *)ptr)[i];

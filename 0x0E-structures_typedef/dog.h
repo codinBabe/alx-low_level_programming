cc481040524842593ad223beda137452bc0290be
codinBabe <teboola2@gmail.com>
2023-07-18 03:02:12 -0700

    add files and task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dog - This is a dog struct
+ * @name:dog name
+ * @age:dog age
+ * @owner:owner details
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

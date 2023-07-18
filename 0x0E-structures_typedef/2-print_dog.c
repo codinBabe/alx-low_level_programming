f7d7694d2fe998139d52ca5dab18dd589646e5d2
codinBabe <teboola2@gmail.com>
2023-07-18 08:34:34 -0700

    fix errors

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,13 +9,17 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
-	printf("Name: %s\n", d->name);
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
-	printf("Age: %f\n", d->age);
+	else
+		printf("Name: %s\n", d->name);
 	if (d->age < 0)
 		printf("Age: (nil)\n");
-	printf("Owner: %s\n", d->owner);
+	else
+		printf("Age: %f\n", d->age);
+
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }

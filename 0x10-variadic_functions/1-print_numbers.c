4df08bcb11d822d45b8e1870b0547fe98d7eb124
codinBabe <teboola2@gmail.com>
2023-07-20 17:08:23 -0700

    added task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - This is a function that prints numbers,
+ * followed by a new line.
+ * @separator:string to be printed between numbers
+ * @n:number of integers passed to the function
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i, nums;
+
+	if (separator == NULL)
+		return;
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		nums = va_arg(args, unsigned int);
+		printf("%d", nums);
+
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+
+	va_end(args);
+	printf("\n");
+}

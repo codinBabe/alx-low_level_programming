bf8a560876c1425c6cc7f9399eb9575f0b795cff
codinBabe <teboola2@gmail.com>
2023-07-25 08:02:04 -0700

    added task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,21 +1,5 @@
 #include "3-calc.h"
 /**
- * _strcmp - This function compare two string
- * @s1: The first string
- * @s2: The second string
- *
- * Return: 0 if s1 == s2, postive number in ascii if s1 > s2,
- * negative number in ascii if s2 > s1.
- */
-int _strcmp(char *s1, char *s2)
-{
-	while (*s1 == *s2 && *s1)
-		++s1, ++s2;
-
-	return (*s1 - *s2);
-}
-
-/**
  * get_op_func - a function that selects the correct function to perform the
  * operation asked by the user.
  * @s: the operator
@@ -36,7 +20,7 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op != NULL)
 	{
-		if (_strcmp(ops[i].op, s) == 0)
+		if (*s == *ops[i].op)
 			return (ops[i].f);
 		i++;
 	}

95b8dd0e08e58d70fda58aa9a1427dec9c32411c
codinBabe <teboola2@gmail.com>
2023-07-27 09:27:59 -0700

    added task 0 and 1

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_list - function that prints all elements of a list_t
+ * @h:head pointer
+ * Return:number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+
+		h = h->next;
+		i++;
+	}
+	return (i);
+}

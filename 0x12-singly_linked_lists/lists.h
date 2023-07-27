4b9ea3110692ece4bfe85f65df341c77aafb6013
codinBabe <teboola2@gmail.com>
2023-07-27 09:46:54 -0700

    fix error in task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
@@ -28,4 +27,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
 #endif

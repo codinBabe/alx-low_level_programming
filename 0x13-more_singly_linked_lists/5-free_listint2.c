bc586b07cc2f37ae598ed91daba3821baa3a173b
codinBabe <teboola2@gmail.com>
2023-08-01 05:19:02 -0700

    add task 4

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,11 +7,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while(*head != NULL)
+	while (*head != NULL)
 	{
 		temp = *head;
 		*head = (*head)->next;
 		free(temp);
 	}
-	*head = NULL;
+	free(*head);
+	*head = 0;
 }

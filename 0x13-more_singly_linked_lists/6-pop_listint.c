a9fc4c91e9a6014413e1690330ce168c43a3a5d8
codinBabe <teboola2@gmail.com>
2023-08-01 06:01:43 -0700

    add task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * pop_listint - a function that deletes the head node of a
+ * listint_t linked list, and returns the head node’s data (n).
+ * @head:node head pointer
+ * Return:head node data
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int data = 0;
+
+	if (*head == NULL)
+	{
+
+		temp = (*head)->next;
+		data = (*head)->n;
+
+		free(*head);
+		*head = temp;
+	}
+
+	return (data);
+}

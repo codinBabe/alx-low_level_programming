a019e52be41587e3a246a3226d06cfdbc1324036
codinBabe <teboola2@gmail.com>
2023-08-01 05:12:34 -0700

    add task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - a function that frees a listint_t list
+ * @head:node head pointer
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while(*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}

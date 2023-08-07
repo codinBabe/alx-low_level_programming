7bd4be32edd21084dd93c8e7765825b7d7937c87
codinBabe <teboola2@gmail.com>
2023-08-07 16:42:45 -0700

    add task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * create_file - a function that creates a file.
+ * @filename:pointer to name of file
+ * @text_content:a NULL terminated string to write to the file
+ * Return:1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int i, O, W;
+
+	if (filename == NULL)
+		return (-1);
+
+	O = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (O == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i]; i++)
+			;
+		W = write(O, text_content, i);
+		if (W == -1)
+		{
+			close(O);
+			return (-1);
+		}
+		else
+		{
+			close(O);
+			return (1);
+		}
+	}
+	else
+	{
+		close(O);
+		return (-1);
+	}
+}

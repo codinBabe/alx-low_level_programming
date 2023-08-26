e1f9f1539a5aa325be1812d126ada92a47dadd4c
codinBabe <teboola2@gmail.com>
2023-08-26 05:39:56 -0700

    fix task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from_file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	buffer = malloc(sizeof(char) * BUFFER_SIZE);

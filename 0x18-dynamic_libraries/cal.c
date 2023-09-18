2197f619d3e4ea26e41f4baab4060c691165b0cf
codinBabe <teboola2@gmail.com>
2023-09-18 05:10:01 -0700

    added sol to task 2

---
+++ 0x18-dynamic_libraries/cal.c
@@ -0,0 +1,61 @@
+#include "cal.h"
+
+/**
+ * add - a function that add 2 numbers
+ * @a: first num
+ * @b: second num
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - a function that add 2 numbers
+ * @a: first num
+ * @b: second num
+ * Return: result
+ */
+
+int sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * mul - a function that mul 2 numbers
+ * @a: first num
+ * @b: second num
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * div - a function that div 2 numbers
+ * @a: first num
+ * @b: second num
+ * Return: result
+ */
+
+int div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * mod - a function that div 2 numbers
+ * @a: first num
+ * @b: second num
+ * Return: result
+ */
+
+int mod(int a, int b)
+{
+        return (a % b);
+}

ae17a70ce820b7979b1da1f7050437a10129da83
codinBabe <teboola2@gmail.com>
2023-09-18 11:04:50 -0700

    fix betty

--- 0x18-dynamic_libraries/makemewin.c
+++ 0x18-dynamic_libraries/makemewin.c
@@ -1,10 +1,14 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+/**
+ * printf - print to stdout
+ * @format:what to print
+ * Return:output
+ */
 int printf(const char *format, ...)
 {
-	write(1, "9 8 10 24 75 - 9\n",17);
+	write(1, "9 8 10 24 75 - 9\n", 17);
 	write(1, "Congratulations, you win the Jackpot!\n", 38);
 	exit(EXIT_SUCCESS);
 }

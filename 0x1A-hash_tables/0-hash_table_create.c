53b7a39be1e2a2a9fb6181acc9d6d169beaf30d7
codinBabe <teboola2@gmail.com>
2023-10-06 15:51:05 -0700

    fix error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,17 +7,17 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new = malloc(sizeof(hash_table_t));
-	hash_node_t **new_array = malloc(sizeof(hash_node_t) * size);
+
+	new->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (!new)
 		return (NULL);
-	if (!new_array)
+	if (!new->array)
 	{
 		free(new);
 		return (NULL);
 	}
 	new->size = size;
-	new->array = new_array;
 
 	return (new);
 }

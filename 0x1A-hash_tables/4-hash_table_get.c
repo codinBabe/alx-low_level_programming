b9485fbf024e06de169df49869f66026213ea405
codinBabe <teboola2@gmail.com>
2023-10-18 13:14:41 -0700

    >>> ht['betty']

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+/**
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	size_t index = key_index((unsigned char *)key, ht->size);
+	hash_node_t *tab = ht->array[index];
+
+	while (tab != NULL)
+	{
+		if(strcmp(tab->key, key) == 0)
+			return (tab->value);
+		tab = tab->next;
+	}
+	return (NULL);
+}
